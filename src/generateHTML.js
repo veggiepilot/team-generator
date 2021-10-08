2c883e341a5c89b4633e68a138f59c7a740a28ec
veggiepilot <johanneschitura@gmail.com>
2021-10-08 10:40:10 -0400

    Updated html file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -4,53 +4,59 @@ let renderEmployees = '';
 let generateHTML = function generateHTML(employeeObjects) {
 
   if (employeeObjects) {
-    console.log(employeeObjects);
     employeeObjects.forEach(employee => {
       employee.getName();
-        console.log(employee.constructor.name);
         switch(employee.constructor.name) {
             case "Manager":
-                renderEmployees += `<div class="card" style="width: 18rem;">
-                <div class="card-body manager">
-                  <h5 class="card-title" id="${employee.name}">${employee.name}</h5>
-                  <p class="card-text" id="${employee.constructor.name}">${employee.constructor.name}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item" id="${employee.id}">ID: ${employee.id}</li>
-                  <li class="list-group-item" id="${employee.email}">Email: <a href="mailto:${employee.email}" >${employee.email}</a></li>
-                  <li class="list-group-item" id="${employee.officeNumber}">Office Number: ${employee.officeNumber}</li>
-                </ul>
-            </div>\n`
+                renderEmployees += 
+            `<div class="card mb-5" style="width: 18rem;">
+              <div class="card-body manager">
+                <h5 class="card-title" id="${employee.name}">${employee.name}</h5>
+                <p class="card-text" id="${employee.constructor.name}">${employee.constructor.name}</p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item" id="${employee.id}">ID: ${employee.id}</li>
+                <li class="list-group-item" id="${employee.email}">Email: <a href="mailto:${employee.email}" >${employee.email}</a></li>
+                <li class="list-group-item" id="${employee.officeNumber}">Office Number: ${employee.officeNumber}</li>
+              </ul>
+            </div>
+
+            `
             break;
 
             case "Engineer":
-                renderEmployees += `<div class="card" style="width: 18rem;">
-                <div class="card-body manager">
-                  <h5 class="card-title" id="${employee.name}">${employee.name}</h5>
-                  <p class="card-text" id="${employee.constructor.name}">${employee.constructor.name}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item" id="${employee.id}"> ID: ${employee.id}</li>
-                  <li class="list-group-item" id="${employee.email}">Email: <a href="mailto: ${employee.email}">${employee.email}</a></li>
-                  <li class="list-group-item" id="${employee.github}">Github: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a></li>
-                </ul>
-            </div> \n`
+                renderEmployees += 
+            `<div class="card mb-5" style="width: 18rem;">
+              <div class="card-body manager">
+                <h5 class="card-title" id="${employee.name}">${employee.name}</h5>
+                <p class="card-text" id="${employee.constructor.name}">${employee.constructor.name}</p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item" id="${employee.id}"> ID: ${employee.id}</li>
+                <li class="list-group-item" id="${employee.email}">Email: <a href="mailto: ${employee.email}">${employee.email}</a></li>
+                <li class="list-group-item" id="${employee.github}">Github: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a></li>
+              </ul>
+            </div>
+
+            `
             break;
 
             case "Intern":
-                renderEmployees += `<div class="card" style="width: 18rem;">
-                <div class="card-body manager">
-                  <h5 class="card-title" id="${employee.name}">${employee.name}</h5>
-                  <p class="card-text" id="${employee.constructor.name}">${employee.constructor.name}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item" id="${employee.id}">ID: ${employee.id}</li>
-                  <li class="list-group-item" id="${employee.email}">Email: <a href="mailto: ${employee.email}" >${employee.email}</a></li>
-                  <li class="list-group-item" id="${employee.school}">School: ${employee.school}</li>
-                </ul>
-            </div> \n`
-            break;
-            
+                renderEmployees += 
+            `<div class="card mb-5" style="width: 18rem;">
+              <div class="card-body manager">
+                <h5 class="card-title" id="${employee.name}">${employee.name}</h5>
+                <p class="card-text" id="${employee.constructor.name}">${employee.constructor.name}</p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item" id="${employee.id}">ID: ${employee.id}</li>
+                <li class="list-group-item" id="${employee.email}">Email: <a href="mailto: ${employee.email}" >${employee.email}</a></li>
+                <li class="list-group-item" id="${employee.school}">School: ${employee.school}</li>
+              </ul>
+            </div>
+
+            `
+            break;    
         }
     })
   }
@@ -64,15 +70,22 @@ let generateHTML = function generateHTML(employeeObjects) {
   
       <!-- Bootstrap CSS -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+      <link rel="stylesheet" href="styles.css">
+
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@200&display=swap" rel="stylesheet">
   
       <title>Winning Team</title>
     </head>
     <body>
       <div class="container">
-          <h1 class="text-center mt-5" >Dream Team</h1>
-          <!-- Manager Card -->
-          ${renderEmployees}
-    </div>
+        <h1 class="text-center mt-5">Winning Team</h1>
+        <div class="cards">
+        ${renderEmployees}
+        </div>
+      </div>
   
       <!-- Option 1: Bootstrap Bundle with Popper -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
